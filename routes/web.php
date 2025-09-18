6305e2643d51042be954cedcaff1876208b5bfd6
saba satseradze <saba.satseradze0@gmail.com>
2025-09-18 20:50:19 +0400

    feat: register routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('api')->name('api.')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth.check')->group(function () {
+        Route::get('/user', [AuthController::class, 'getUser']);
+    });
 });
